89e11036843e43a279edc6926a4e59aed62a44ad
M-Struwe <michel_seje@hotmail.com>
2026-05-07 10:36:54 +0200

    Oprettelse af patient og adresse

--- src/BookRight.Domain/Entities/Patient.cs
+++ src/BookRight.Domain/Entities/Patient.cs
@@ -1,6 +1,15 @@
-﻿namespace BookRight.Domain.Entities;
+﻿using BookRight.Domain.ValueObjects;
 
-public class Patient
+namespace BookRight.Domain.Entities;
+
+public class Patient : AggregateRoot
 {
-    
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string PhoneNumber { get; private set; }
+    public string Email { get; private set; }
+    public DateTime Birthday { get; private set; }
+    public Address PatientAddress { get; private set; }
+    public string Note { get; private set; }
+    public Guid PreferedPractitioner { get; private set; }
 }
---
+++ src/BookRight.Domain/ValueObjects/Address.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookRight.Domain.ValueObjects
+{
+    public record Address  //Patient og klinik skal have adresse
+    {
+        public string StreetName { get; private set; }
+        public int Zipcode { get; private set; }
+    }
+}
