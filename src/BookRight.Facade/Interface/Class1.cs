89254c2e9201390a4870b29a764d4875419cfe4f
Virtou <143692063+Virtou2021@users.noreply.github.com>
2026-05-07 09:50:22 +0200

    Oprettet dummy filer

---
+++ src/BookRight.Facade/Interface/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookRight.Facade.Interface
+{
+    internal class Class1
+    {
+    }
+}
---
+++ src/BookRight.UseCases/Repositories/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookRight.UseCases.Repositories
+{
+    internal class Class1
+    {
+    }
+}
