82941ba77b820cad19ee2910e90db3ba4a9c09b4
M-Struwe <michel_seje@hotmail.com>
2026-05-15 12:24:59 +0200

    Oprettelse af TreatmenttypeQueriesImpl samt tilføjelse af TreatmentType i DbContext

---
+++ src/BookRight.Infrastructure/Query/TreatmentTypeQueriesImpl.cs
@@ -0,0 +1,42 @@
+﻿using BookRight.Domain.Entities;
+using BookRight.Facade.DTO;
+using BookRight.Facade.Interfaces.Queries;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookRight.Infrastructure.Query
+{
+    public class TreatmentTypeQueriesImpl : ITreatmentTypeQueries
+    {
+        private readonly AppDbContext _db;
+
+        public TreatmentTypeQueriesImpl(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        async Task<IReadOnlyList<TreatmentTypeDto>> ITreatmentTypeQueries.GetAllAsync()
+        {
+            return await _db.TreatmentTypes.AsNoTracking().Select(t => new TreatmentTypeDto(
+               t.Id,
+               t.Name,
+               t.AuthorizationType.ToString(),
+               t.MaxParticipants
+                )).ToListAsync();
+        }
+
+        async Task<TreatmentTypeDto?> ITreatmentTypeQueries.GetAsync(Guid id)
+        {
+            return await _db.TreatmentTypes.AsNoTracking()
+                .Where(t => t.Id == id).Select(t => new TreatmentTypeDto(
+                    t.Id,
+                    t.Name,
+                    t.AuthorizationType.ToString(),
+                    t.MaxParticipants
+                    )).FirstOrDefaultAsync();
+        }
+
+    }
+}
