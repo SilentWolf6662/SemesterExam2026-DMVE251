5b35a11d1bce543d8933857bd17c4fccb509de4c
Virtou <143692063+Virtou2021@users.noreply.github.com>
2026-05-18 10:08:15 +0200

    Oprettet ChangeStatus så man kan ændre status på en eksisterende appointment

--- src/BookRight.Facade/Command/ChangeStatusRequest.cs
+++ src/BookRight.Facade/Command/ChangeStatusRequest.cs
@@ -1,3 +1,3 @@
 ﻿namespace BookRight.Facade.Command;
 
-public record ChangeStatusRequest(Guid AppointmentId);
+public record ChangeStatusRequest(Guid AppointmentId, string Note, string Status);
--- src/BookRight.UseCases/Command/ChangeStatus.cs
+++ src/BookRight.UseCases/Command/ChangeStatus.cs
@@ -1,10 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using BookRight.Domain.Exceptions;
+using BookRight.Facade.Command;
+using BookRight.Facade.Interfaces.UseCase;
+using BookRight.UseCases.Repositories;
 
-namespace BookRight.UseCases.Command
+namespace BookRight.UseCases.Command;
+
+public class ChangeStatus : IChangeStatus
 {
-    internal class ChangeStatus
+    private readonly IAppointmentRepository _appointmentRepo;
+
+    public ChangeStatus(IAppointmentRepository appointmentRepo)
     {
+        _appointmentRepo = appointmentRepo;
+    }
+
+    public async Task Execute(ChangeStatusRequest request)
+    {
+        var booking = await _appointmentRepo.GetByIdAsync(request.AppointmentId);
+
+        // Hvis aftalen ikke findes, kaster vi en NotFoundException
+        if (booking == null) throw new NotFoundException("Appointment not found"); 
+
+        switch (request.Status.ToLower())
+        {
+            case "cancelled":
+                booking.Cancel(); // Sæt status til "cancelled" og fjern eventuelle noter
+                break;
+            case "completed":
+                booking.Complete(request.Note); // Sæt status til "completed" og opdater noten
+                break;
+            case "nooneshowed":
+                booking.NoOneShowed(); // Sæt status til "nooneshowed" og fjern eventuelle noter
+                break;
+            default:
+                throw new ArgumentException("Invalid status");
+        }
+
+        await _appointmentRepo.SaveAsync();
     }
 }
