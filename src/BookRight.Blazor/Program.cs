3485cd482786d6cb3de19e1775d85322ed911411
SilentWolf6662 <52424766+SilentWolf6662@users.noreply.github.com>
2026-05-19 14:53:40 +0200

    Lavet udkast til kalender oversigt og lagt den i overview page

--- src/BookRight.Blazor/Program.cs
+++ src/BookRight.Blazor/Program.cs
@@ -2,61 +2,60 @@ using BookRight.Blazor.Components;
 using BookRight.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
-namespace BookRight.Blazor
+namespace BookRight.Blazor;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+        builder.Services.AddInfrastructure(builder.Configuration);
+
+        var app = builder.Build();
+
+        if (app.Configuration.GetValue<bool>("SeedData"))
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddRazorComponents()
-                .AddInteractiveServerComponents();
-
-            builder.Services.AddInfrastructure(builder.Configuration);
-
-            var app = builder.Build();
-
-            if (app.Configuration.GetValue<bool>("SeedData"))
+            using var scope = app.Services.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            if (db.Clinics.Any())
             {
-                using var scope = app.Services.CreateScope();
-                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-                if (db.Clinics.Any())
-                {
-                    db.Appointments.ExecuteDelete();
-                    db.Patients.ExecuteDelete();
-                    db.Practitioners.ExecuteDelete();
-                    db.TreatmentTypes.ExecuteDelete();
-                    db.Clinics.ExecuteDelete();
-                }
-                new SeedData().Initialize(db);
+                db.Appointments.ExecuteDelete();
+                db.Patients.ExecuteDelete();
+                db.Practitioners.ExecuteDelete();
+                db.TreatmentTypes.ExecuteDelete();
+                db.Clinics.ExecuteDelete();
             }
-            else
-            {
-                using var scope = app.Services.CreateScope();
-                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-                new SeedData().Initialize(db);
-            }
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-            app.UseHttpsRedirection();
-
-            app.UseAntiforgery();
-
-            app.MapStaticAssets();
-            app.MapRazorComponents<App>()
-                .AddInteractiveServerRenderMode();
-
-            app.Run();
+            new SeedData().Initialize(db);
         }
+        else
+        {
+            using var scope = app.Services.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            new SeedData().Initialize(db);
+        }
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+        app.UseHttpsRedirection();
+
+        app.UseAntiforgery();
+
+        app.MapStaticAssets();
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.Run();
     }
 }
